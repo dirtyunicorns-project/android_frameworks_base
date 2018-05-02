3b711e3cfe068f46cba8910d35c856705ef32a41
ezio84 <brabus84@gmail.com>
2018-05-02 18:24:50 +0200

    Blacklist apps in the ChooserAcitivity (e.g. Share button) [1/2]
    todo:
    - maybe add an observer (but it seems not worth it here)
    - possibile code cleanup
    - check if anything gets broken by this :P
    
    Change-Id: I3914c95ee24f8d505ecc299d7c3c1bc130f7ea35

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4742,6 +4742,12 @@ public final class Settings {
         public static final String LISTVIEW_INTERPOLATOR = "listview_interpolator";
 
         /**
+         * Apps to hide in the ChooserActivity
+         * @hide
+         */
+        public static final String CHOOSER_ACTIVITY_BLACKLIST = "chooser_activity_blacklist";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
